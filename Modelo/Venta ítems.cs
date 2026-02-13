5430039c34ca0a36391a5a2ae0ccb2d801e90914
Lisandrofor <58186709+Lisandrofor@users.noreply.github.com>
2026-02-13 09:28:14 -0300

    Crear VentaItems.cs

---
+++ Modelo/Venta ítems.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+
+public class VentaItem
+{
+    public Producto Producto { get; private set; }
+    public int Cantidad { get; private set; }
+    public decimal PrecioUnitario { get; private set; }
+    public decimal Descuento { get; private set; } // descuento por item
+
+    public VentaItem(Producto producto, int cantidad, decimal precioUnitario)
+    {
+        Producto = producto;
+        Cantidad = cantidad;
+        PrecioUnitario = precioUnitario;
+    }
+
+    public decimal Subtotal()
+    {
+        return (PrecioUnitario * Cantidad) - Descuento;
+    }
+
+    public decimal CalcularImpuesto()
+    {
+        return Subtotal() * Producto.Impuesto.Porcentaje;
+    }
+
+    public decimal Total()
+    {
+        return Subtotal() + CalcularImpuesto();
+    }
+
+ }
+}
+
+
+
+
+
+
