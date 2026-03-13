6aa906bec7bf7119d1ac53c256150319f6275eac
Lisandrofor <58186709+Lisandrofor@users.noreply.github.com>
2026-03-13 09:29:05 -0300

    Actualizar VentaItems.cs

--- Modelo/VentaItems.cs
+++ Modelo/VentaItems.cs
@@ -56,8 +56,7 @@ public class VentaItems : INotifyPropertyChanged
             }
 
 
-        public VentaItems(Producto producto, int cantidad, decimal precioUnitario, Impuesto imp) { Producto = producto.Nombre; Cantidad = cantidad; PrecioUnitario = producto.Precio; Impue = imp; }
-
+        public VentaItems(Producto producto, int cantidad, decimal precioUnitario, Impuesto imp) { Producto = producto.nombre; Cantidad = cantidad; PrecioUnitario = producto.precio; Impue = imp; }
         public decimal Subtotal
                 => (PrecioUnitario * Cantidad) - Descuento;
 
