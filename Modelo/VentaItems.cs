5b8b5921edd9ac796c554d3e6fdea1d7ba5c3bd0
Lisandrofor <58186709+Lisandrofor@users.noreply.github.com>
2026-03-17 12:35:20 -0300

    Actualizar VentaItems.cs

--- Modelo/VentaItems.cs
+++ Modelo/VentaItems.cs
@@ -39,7 +39,20 @@ public class VentaItems : INotifyPropertyChanged
         public decimal Precio { get; set; }
             public decimal Descuento { get; set; }
 
-            public int IdImpuesto => Impue?.Id ?? 0;
+private int _idImpuesto;
+public int IdImpuesto
+{
+    get => _idImpuesto;
+    set
+    {
+        _idImpuesto = value;
+
+        // 🔥 sincronizar el objeto Impuesto
+        Impue = listaImpuestos.FirstOrDefault(i => i.Id == value);
+
+        OnPropertyChanged(nameof(IdImpuesto));
+    }
+}
 
             public Impuesto Impue
             {
