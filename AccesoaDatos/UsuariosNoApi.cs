ea4b78f2e56211c5217e5691c856ff13df9c3cd3
Lisandrofor <58186709+Lisandrofor@users.noreply.github.com>
2026-03-27 07:33:50 -0300

    Actualizar UsuariosNoApi.cs

--- AccesoaDatos/UsuariosNoApi.cs
+++ AccesoaDatos/UsuariosNoApi.cs
@@ -1,16 +1,16 @@
 using System.IO;
 using System.Text.Json;
 
-public List<Loginuser> ObtenerUsuariosMock()
+public List<Usuario> ObtenerUsuariosMock()
 {
     string path = "usuarios.json";
 
     if (!File.Exists(path))
-        return new List<Loginuser>();
+        return new List<Usuario>();
 
     string json = File.ReadAllText(path);
 
-    var usuarios = JsonSerializer.Deserialize<List<Loginuser>>(json);
+    var usuarios = JsonSerializer.Deserialize<List<Usuario>>(json);
 
-    return usuarios ?? new List<Loginuser>();
+    return usuarios ?? new List<Usuario>();
 }
